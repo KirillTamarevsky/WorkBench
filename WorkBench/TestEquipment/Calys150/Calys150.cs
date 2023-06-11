4d4e7cde69a83708f7409488e5eb507d3da65695
Kirill Tamarevsky <tamarevsky@hotmail.com>
2023-06-11 15:51:03 +0300

    initial calys150 commit

---
+++ WorkBench/TestEquipment/Calys150/Calys150.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkBench.Interfaces;
+using WorkBench.Interfaces.InstrumentChannel;
+
+namespace WorkBench.TestEquipment.Calys150
+{
+    public class Calys150 : IInstrument
+    {
+        public IInstrumentChannel[] Channels => throw new NotImplementedException();
+
+        public bool IsOpen => throw new NotImplementedException();
+
+        public bool Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
