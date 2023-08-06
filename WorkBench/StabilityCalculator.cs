aed22a90959276ae4a1def4ab8bae522cc73757f
Kirill Tamarevsky <tamarevsky@hotmail.com>
2023-08-06 11:01:25 +0300

    stability calculator add thread safety

--- WorkBench/StabilityCalculator.cs
+++ WorkBench/StabilityCalculator.cs
@@ -13,10 +13,37 @@ namespace WorkBench
         public readonly List<OneMeasure> Measures;
 
         readonly List<OneMeasure> StableMeasures;
-        public int MeasuresCount { get => Measures.Count; }
+        public int MeasuresCount
+        {
+            get
+            {
+                lock (Measures)
+                {
+                    return Measures.Count;
+                }
+            }
+        }
         public int MinMeasuresCount { get; set; }
-        public bool Ready { get => StableMeasures.Count >= MinMeasuresCount; }
-        public double StableMeanValue { get => StableMeasures.Average(m => m.Value); }
+        public bool Ready
+        {
+            get
+            {
+                lock (StableMeasures)
+                {
+                    return StableMeasures.Count >= MinMeasuresCount;
+                }
+            }
+        }
+        public double StableMeanValue
+        {
+            get
+            {
+                lock (StableMeasures)
+                {
+                    return StableMeasures.Average(m => m.Value);
+                }
+            }
+        }
         public TimeSpan MeasuringTimeSpan { get; private set; }
         public TimeSpan MinTimeToStabilize { get; set; }
 
@@ -36,64 +63,75 @@ namespace WorkBench
         }
         public void Reset()
         {
-            TrendStatus = TrendStatus.Unknown;
-            Measures.Clear();
-            StableMeasures.Clear();
-            MeasuringTimeSpan = TimeSpan.FromSeconds(0);
+            lock (Measures)
+            {
+                lock (StableMeasures)
+                {
+                    TrendStatus = TrendStatus.Unknown;
+                    Measures.Clear();
+                    StableMeasures.Clear();
+                    MeasuringTimeSpan = TimeSpan.FromSeconds(0);
+                }
+            }
         }
         public void AddMeasure(OneMeasure oneMeasure)
         {
-            if (oneMeasure == null) return;
-            Measures.Add(oneMeasure);
-            DateTime mintimestamp;
-            TimeSpan timespan;
-
-            mintimestamp = Measures.Min(cm => cm.TimeStamp); 
-            timespan = DateTime.Now - mintimestamp;
-
-            while (timespan.TotalSeconds > MinTimeToStabilize.TotalSeconds & Measures.Count > MinMeasuresCount)
+            lock (Measures)
             {
-                Measures.RemoveAt(0);
-                mintimestamp = Measures.Min(cm => cm.TimeStamp);
-                timespan = DateTime.Now - mintimestamp;
-
-            }
-
-            MeasuringTimeSpan = timespan;
-
-            MeanValue = Measures.Average((om) => om.Value);
-
-            StdDeviation = Math.Sqrt(Measures.Sum(meas => Math.Pow( meas.Value - MeanValue, 2))) ;
-
-            LRSlopeCalc();
-
-            if (MeasuresCount >= MinMeasuresCount)
-            {
-
-                if (Math.Round(LRSlope, 3, MidpointRounding.ToZero) == 0)
+                lock (StableMeasures)
                 {
-                    TrendStatus = TrendStatus.Stable;
-                    StableMeasures.Add(oneMeasure);
-                }
-                else
-                {
-                    StableMeasures.Clear();
-                    if (LRSlope < 0)
-                    {
-                        TrendStatus = TrendStatus.FallDown;
-                    }
-                    else if (LRSlope > 0)
-                    {
-                        TrendStatus = TrendStatus.GrowUP;
-                    }
-                }
-            }
-            else
-            {
-                StableMeasures.Clear();
-                TrendStatus = TrendStatus.Unknown;
-            }
+                    if (oneMeasure == null) return;
+                    Measures.Add(oneMeasure);
+                    DateTime mintimestamp;
+                    TimeSpan timespan;
 
+                    mintimestamp = Measures.Min(cm => cm.TimeStamp);
+                    timespan = DateTime.Now - mintimestamp;
+
+                    while (timespan.TotalSeconds > MinTimeToStabilize.TotalSeconds & Measures.Count > MinMeasuresCount)
+                    {
+                        Measures.RemoveAt(0);
+                        mintimestamp = Measures.Min(cm => cm.TimeStamp);
+                        timespan = DateTime.Now - mintimestamp;
+
+                    }
+
+                    MeasuringTimeSpan = timespan;
+
+                    MeanValue = Measures.Average((om) => om.Value);
+
+                    StdDeviation = Math.Sqrt(Measures.Sum(meas => Math.Pow(meas.Value - MeanValue, 2)));
+
+                    LRSlopeCalc();
+
+                    if (MeasuresCount >= MinMeasuresCount)
+                    {
+
+                        if (Math.Round(LRSlope, 3, MidpointRounding.ToZero) == 0)
+                        {
+                            TrendStatus = TrendStatus.Stable;
+                            StableMeasures.Add(oneMeasure);
+                        }
+                        else
+                        {
+                            StableMeasures.Clear();
+                            if (LRSlope < 0)
+                            {
+                                TrendStatus = TrendStatus.FallDown;
+                            }
+                            else if (LRSlope > 0)
+                            {
+                                TrendStatus = TrendStatus.GrowUP;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        StableMeasures.Clear();
+                        TrendStatus = TrendStatus.Unknown;
+                    }
+                }
+            }
         }
         private void LRSlopeCalc()
         {
